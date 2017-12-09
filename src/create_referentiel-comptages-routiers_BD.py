ec0174eed757a26cc8c90f34d274443d64675c47
raphaelauv <raphaelauv@users.noreply.github.com>
2017-12-09 23:05:05 +0100

    Try direct parse in ptyhon VS pandas

--- src/create_referentiel-comptages-routiers_BD.py
+++ src/create_referentiel-comptages-routiers_BD.py
@@ -183,6 +183,10 @@ def parseHour(x):
 		return np.nan
 	'''
 
+
+'''
+return number of the day in the week , monday=0 ...
+'''
 def getDay_DATE(date):
 	tab= date.split("-")
 	year = int(tab[0])
@@ -190,6 +194,9 @@ def getDay_DATE(date):
 	day = int(tab[2])
 	return datetime(year,month,day).weekday()
 
+'''
+return number of the week in the year
+'''
 def getWeek_DATE(date):	
 	tab= date.split("-")
 	year = int(tab[0])
@@ -199,13 +206,11 @@ def getWeek_DATE(date):
 
 
 def getTupleWeek_Day(date):
-	print(date)
 	tab= date.split("-")
 	year = int(tab[0])
 	month = int(tab[1])
 	day = int(tab[2])
 	mydate = datetime(year,month,day)
-	print(mydate.weekday() , mydate.isocalendar()[1])
 	return (mydate.weekday() , mydate.isocalendar()[1])
 
 
@@ -218,7 +223,7 @@ def exemple(x):
 	x['dateNumber']= mytuple[1]
 
 def editChunk(chunk):
-	chunk=chunk.assign(
+	chunk=chunk.	assign(
 		hour=chunk.theHour.apply(lambda x:parseHour(x)) ,
 		year=chunk.date.apply(lambda x:parseYear(x)) ,
 		dateNumber = chunk.date.apply(lambda x: getDay_DATE(x)),
@@ -237,6 +242,70 @@ def getPositions(pathName):
 	return pd.read_csv(pathName, header=None , delimiter=r"\s+",dtype={'date':'str','theHour':'str'},
 			chunksize=50000,names=["id_arc_trafics", "date","theHour", "debit", "taux_occ"],decimal=',')
 
+
+'''
+direct parse of data
+'''
+def getTupleWithData(line):
+	line = line[:-1]
+	arrayLine =line.split("\t")
+	#print(arrayLine)
+	id_arc_trafics = int(arrayLine[0])
+
+	strYear = arrayLine[1].split(" ")
+	
+	year = parseYear(strYear[0])
+
+	typleDate = getTupleWeek_Day(strYear[0])
+
+	dateNumber = typleDate[0]
+	numberWeek = typleDate[1]
+
+	hour = parseHour(strYear[1])
+	
+	debit =-1
+	if(len(arrayLine)>3):
+		if(len(arrayLine[2])>0):
+			#print("str->"+str(arrayLine[2]))
+			val=arrayLine[2].replace(",",".")
+			debit =float(val)
+
+	taux_occ=-1
+	if(len(arrayLine)>4):
+		if(len(arrayLine[3])>0):
+			val=arrayLine[3].replace(",",".")
+			#print("->"+str(arrayLine[3]))
+			taux_occ = float(val)
+		
+
+	return (id_arc_trafics,year,numberWeek,dateNumber,hour,debit,taux_occ)
+
+
+'''
+test performance of direct parsing in python
+'''
+def createBulk(namePath,conn):
+	with open(namePath) as infile:
+		array=[]
+		cmp=0
+		cmpT=0
+		for line in infile:
+			#print(cmpT)
+			cmpT+=1
+			array.append(getTupleWithData(line))
+			
+			if(cmp==1000):
+				cur = conn.cursor()
+				cur.execute('BEGIN TRANSACTION')
+				cur.execute('COMMIT')
+				cmp=0
+
+		if(cmp>0):
+			cur = conn.cursor()
+			cur.execute('BEGIN TRANSACTION')
+			cur.execute('COMMIT')
+
+
 from concurrent import futures
 def parseFolderDataTraffic(startYear,numberOfYears,numberOfMonth):
 
@@ -246,12 +315,13 @@ def parseFolderDataTraffic(startYear,numberOfYears,numberOfMonth):
 	import multiprocessing
 
 	nbThreads = multiprocessing.cpu_count()
-	executor = futures.ProcessPoolExecutor(max_workers=nbThreads+2)
+	executor = futures.ProcessPoolExecutor(max_workers=nbThreads)
 
 	for year in tqdm(range(startYear,startYear+numberOfYears)):
 		for month in tqdm(range(1,numberOfMonth+1)):
 			listOfFuturs=[]
 			pathName =getPathFile(strNameFolder ,year ,month)
+			
 			try:
 				positions = getPositions(pathName)
 			except FileNotFoundError :
@@ -260,10 +330,10 @@ def parseFolderDataTraffic(startYear,numberOfYears,numberOfMonth):
 
 			for chunk in positions:
 				listOfFuturs.append(executor.submit(editChunk,chunk))
-
 			for fut in tqdm(futures.as_completed(listOfFuturs), total=len(listOfFuturs), desc=pathName):
 				putToDB(fut.result(),conn)
 			conn.commit()
+			
 	conn.close()
 	executor.shutdown()
 
@@ -273,11 +343,16 @@ def putToDB(chunk,conn):
 
 #modeBD()
 #modeDict()
+conn = sql.connect('Blabla.db')
+#createBulk("traffic/2013_paris_donnees_trafic_capteurs/donnees_trafic_capteurs_201301.txt",conn)
 print("\n")
-parseFolderDataTraffic(2013,1,12)
+
+
+parseFolderDataTraffic(2013,1,1)
 
 print("\n")
-conn = sql.connect('Blabla.db')
-#testBD_BLABLA(conn)
+
+
+testBD_BLABLA(conn)
 print("FINISH")
 #modeDict()
